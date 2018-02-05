98bd771550c655c20aa5cb4122f764a5879dce72
megan.duffy <monster13>
2018-02-05 14:59:34 +0000

    Created new branch "megan" and changed the information that quickstart.py displays

--- tornado/views.py
+++ tornado/views.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from __future__ import print_function
 
 from django.shortcuts import render
 
 
 def get_calendar(request):
     return render(request, 'index.html')
+
+
