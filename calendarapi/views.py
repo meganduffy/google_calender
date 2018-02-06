4ba53ec93712729489f6d0e0d9c4c68f19c26879
TurnUpTheMike <michael.pantaleano@wpengine.com>
2018-02-06 19:24:52 +0000

    add function to views

--- calendarapi/views.py
+++ calendarapi/views.py
@@ -2,5 +2,26 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from quickstart import create_event
+from attendees import Attendees
+
 
 # Create your views here.
+def prepare_create_event(request):
+    """
+    Where do these variables come from?
+    :return: 
+    """
+    # Test variables
+    summary = 'Test event sum'
+    location = 'Test event location'
+    description = 'Test event desc'
+    start_time_value = '2018/02/05 21:00:00'
+    end_time_value = '2018/02/05 22:00:00'
+    start_time = datetime.datetime.strptime(start_time_value, "%Y/%m/%d %H:%M:%S")
+    end_time = datetime.datetime.strptime(end_time_value, "%Y/%m/%d %H:%M:%S")
+    attendees = Attendees()
+    attendees.add_attendee("daragh.t.lowe@gmail.com")
+
+    # Calls the create_event function and passes variables that will be passed into it from django
+    create_event(summary, location, description, start_time, end_time, attendees)
